a7619d9208a94e9a950c85fa50556a20068bc3e6
John Scott <jfscott@memphis.edu>
2024-05-01 15:42:46 -0500

    trying again

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,14 @@
 class PagesController < ApplicationController
     def home
+        render :home
     end
     def about
-
+        render :about
     end
     def work
-
+        render :work
     end
     def contact
-        
+        render :contact
     end
 end
